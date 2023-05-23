a294cace8f1d1374b12542c9b4a744af7d647af9
engy7 <engyayousef@gmail.com>
2023-05-23 16:02:31 +0300

    Detecting Collision Displaying Game Over

--- gameClient.py
+++ gameClient.py
@@ -9,134 +9,119 @@ import queue
 import random
 from time import sleep
 
-clock=pygame.time.Clock()
-
 
 # VM: 20.199.99.151
 # serverIP = '20.199.99.151'
 serverIP = '127.0.0.1'
 serverPort = 20000
 
-# connect to server
+# connect to server for game
 clientSock_game = socket(AF_INET, SOCK_STREAM)
 clientSock_game.connect((serverIP, serverPort))
-
+# connect to server for chat
 clientSock_chat = socket(AF_INET, SOCK_STREAM)
 clientSock_chat.connect((serverIP, serverPort+20))
 
 # get the nickname from the player
-msg=tkinter.Tk()
+msg = tkinter.Tk()
 msg.withdraw()
 nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
 clientSock_chat.send(nickname.encode())
 print(nickname)
 
-# initialize pygame
+# initialize pygame and form window
 pygame.init()
-screenWidth = 800
+screenWidth = 1200
 screenHeight = 600
+gameDisplay = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption('Racing Multiplayer Game')
 
 # load background images
 background = 'img/back_ground1.jpg'
 car = 'img/car.png'
-enemy='img/enemy_car_1.png' 
-
+enemy = 'img/enemy_car_1.png' 
 bgImg = pygame.image.load(background)
 carImg = pygame.image.load(car)
-enemyImg=pygame.image.load(enemy)
+enemyImg = pygame.image.load(enemy)
 
 # background variables
 bg_y1 = 0
 bg_y2 = -screenHeight
-bg_speed = 3
+bg_speed = 2
+limit1 = 15
+limit2 = bgImg.get_width()-carImg.get_width()-15
 
-
+# player car variables
+location = [0,0]
+car_bottom_offset = 10
+car_speed = 1
 
 #enemy car variables
-enemy_car_startx = random.randrange(310, 450)
-enemy_car_starty1 = 0
+random_x = 0
+enemy_y = screenHeight
 enemy_car_starty2 = -screenHeight
 enemy_car_speed = 1
 enemy_car_width = 49
 enemy_car_height = 10
 
-
-# car variables
-location = [0,0]
-car_bottom_offset = 10
-car_speed = 1.5
-
-#counting score
-count=0
+# score variables
+count = 0
 white = (255, 255, 255)
 black = (0, 0, 0)
+
+# queues
+messageQueue = queue.Queue()
+textAreaQueue = queue.Queue()
 
 # shows the car for a given road
 def showCar(bg_x, location_x, location_y):
     car_x = bg_x + bgImg.get_width()/2 - carImg.get_width()/2 + location_x
     car_y = bgImg.get_height() - carImg.get_height() - car_bottom_offset + location_y
-    if car_x < bg_x + 15:
-        car_x = bg_x + 15
-    if car_x > bg_x + bgImg.get_width() - carImg.get_width() - 15:
-        car_x = bg_x + bgImg.get_width() - carImg.get_width() - 15
+    if car_x < bg_x + limit1:
+        car_x = bg_x + limit1
+    if car_x > bg_x + limit2:
+        car_x = bg_x + limit2
     gameDisplay.blit(carImg, (car_x, car_y))
-
-'''
-#shows the enemy car
-def showEnemyCar(enemy_car_startx, enemy_car_starty):
-    if enemy_car_starty > screenHeight:
-        enemy_car_starty = 0 - enemy_car_height
-        enemy_car_startx = random.randrange(310, 450)
-    gameDisplay.blit(enemyImg, (enemy_car_startx, enemy_car_starty))
-    enemy_car_starty += enemy_car_speed
-    gameDisplay.blit(enemyImg, (enemy_car_startx, enemy_car_starty))
-'''
-
+    return car_x, car_y
 
 #shows the enemy car
-def showEnemyCar(bg_x,enemy_car_startx,enemy_car_starty1,enemy_car_starty2):
-    
-    gameDisplay.blit(enemyImg, (enemy_car_startx, enemy_car_starty1))
-    enemy_car_starty1 += enemy_car_speed
-    gameDisplay.blit(enemyImg, (enemy_car_startx, enemy_car_starty2))
-    enemy_car_starty2 += enemy_car_speed
-    
-    random_location_x = random.randrange(-100, 100)
-
-    if enemy_car_starty1 >= screenHeight:
-        enemy_car_starty1 = -bgImg.get_height()
-        enemy_car_startx = bg_x + bgImg.get_width()/2 - enemyImg.get_width()/2 + random_location_x
-    if enemy_car_starty2 >= screenHeight:
-        enemy_car_starty2 = -bgImg.get_height()
-        enemy_car_startx = bg_x + bgImg.get_width()/2 - enemyImg.get_width()/2 + random_location_x
-    return enemy_car_startx, enemy_car_starty1,enemy_car_starty2
-    
-
+def showEnemyCar(bg_x, random_x, enemy_y):
+    if enemy_y >= screenHeight:
+        enemy_y = -bgImg.get_height()
+        random_x = random.randrange(limit1, limit2)
+    enemy_x = bg_x + random_x
+    gameDisplay.blit(enemyImg, (enemy_x, enemy_y))
+    enemy_y += enemy_car_speed
+    return random_x, enemy_y
 
 # shows the road of a given player index
 def showMovingRoad(bg_y1, bg_y2, playersCnt, idx):
     bg_x = (screenWidth/2) - playersCnt*(bgImg.get_width()/2) + idx*(bgImg.get_width())
     gameDisplay.blit(bgImg, (bg_x, bg_y1))
-    bg_y1 += enemy_car_speed
+    bg_y1 += bg_speed
     gameDisplay.blit(bgImg, (bg_x, bg_y2))
-    bg_y2 += enemy_car_speed
+    bg_y2 += bg_speed
     if bg_y1 >= screenHeight:
         bg_y1 = -bgImg.get_height()
     if bg_y2 >= screenHeight:
         bg_y2 = -bgImg.get_height()
     return bg_x, bg_y1, bg_y2
 
+def detectCollision(car_x, car_y, enemy_x, enemy_y):
+    if car_x > enemy_x and car_x < enemy_x+enemyImg.get_width():
+        if car_y > enemy_y and car_y < enemy_y+enemyImg.get_height():
+            return True
+        if car_y+carImg.get_height() > enemy_y and car_y+carImg.get_height() < enemy_y+enemyImg.get_height():
+            return True
+    if car_x+carImg.get_width() > enemy_x and car_x+carImg.get_width() < enemy_x+enemyImg.get_width():
+        if car_y > enemy_y and car_y < enemy_y+enemyImg.get_height():
+            return True
+        if car_y+carImg.get_height() > enemy_y and car_y+carImg.get_height() < enemy_y+enemyImg.get_height():
+            return True
+        
+    return False
 
-def display_message(msg):
-    font = pygame.font.SysFont("comicsansms", 72, True)
-    text = font.render(msg, True, (255, 255, 255))
-    gameDisplay.blit(text, (400 - text.get_width() // 2, 240 - text.get_height() // 2))
-    #display_credit()
-    pygame.display.update()
-    clock.tick(60)
-    sleep(1)
-
-
+# write a message to the chat text area
 def updateTextArea(text_area, message):
     if message != ' ':
         text_area.config(state = 'normal')
@@ -145,27 +130,21 @@ def updateTextArea(text_area, message):
         text_area.yview('end')
         text_area.config(state='disabled')
 
-'''
-def highscore(count):
-        if count%1000==0:
-            font1 = pygame.font.SysFont("arial", 20)
-            text1 = font1.render("Score : " + str(count-1000), True, black)
-            gameDisplay.blit(text1, (0, 0))
-            font2 = pygame.font.SysFont("arial", 20)
-            text2 = font2.render("Score : " + str(count), True, white)
-            gameDisplay.blit(text2, (0, 0))
-        return count
-'''
+# view the score
 def highscore(count):
     font2 = pygame.font.SysFont("arial", 20)
     text2 = font2.render("Score : " + str(int(count/100)*100), True, white)
     gameDisplay.blit(text2, (0, 0))
     return count
 
+def display_message(msg):
+        font = pygame.font.SysFont("comicsansms", 72, True)
+        text = font.render(msg, True, (255, 255, 255))
+        gameDisplay.blit(text, (600 - text.get_width() // 2, 240 - text.get_height() // 2))
+        pygame.display.update()
+        sleep(1)
 
-messageQueue = queue.Queue()
-textAreaQueue = queue.Queue()
-# chat
+# GUI thread
 def gui():
     win = tkinter.Tk()
     win.configure(bg="lightgray")
@@ -192,9 +171,8 @@ def gui():
         input_area.delete('1.0','end')
 
     def stop():
-        run_chat= False
         win.destroy()
-        clientSock_game.close()
+        clientSock_chat.close()
         exit(0)
 
     send_button = tkinter.Button(win, text= "Send",command=write)
@@ -204,18 +182,15 @@ def gui():
     win.protocol("WM_DELETE_WINDOW",stop)
     win.mainloop()
 
-# GUI thread
+# make GUI thread
 guiThread = threading.Thread(target=gui)
 guiThread.start()
 
-# form the game display window
-gameDisplay = pygame.display.set_mode((screenWidth, screenHeight))
-pygame.display.set_caption('Racing Multiplayer Game')
-
-# game
+# Game and Chat main thread
 run = True
-crash=False
+iteration=False
 while run:
+    
     # if the user exits the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -225,7 +200,7 @@ while run:
         pygame.display.quit()
         pygame.quit()
         clientSock_game.close()
-        clientSock_game.close()
+        clientSock_chat.close()
         break
     
     # if the user presses arrow keys
@@ -235,16 +210,15 @@ while run:
     if keys[pygame.K_RIGHT]:
         location[0] += car_speed
 
-
-
     # send the current player location
     clientSock_game.send(pickle.dumps(location))
-
     # receive all players
     players = pickle.loads(clientSock_game.recv(4096))
-
     
-
+    if iteration==False:
+        my_idx=len(players)-1
+        iteration=True
+        print(str(my_idx))
     # get chat message
     if not messageQueue.empty():
         chatMessage = messageQueue.get()
@@ -252,7 +226,6 @@ while run:
         chatMessage = ' '
     clientSock_chat.send(chatMessage.encode())
     broadcastMessage = clientSock_chat.recv(4096).decode()
-    # broadcastMessage = broadcastMessage.replace(' ', '')
     broadcastMessage = broadcastMessage.strip()
     broadcastMessage += '\n'
     if broadcastMessage != ' ' and broadcastMessage != '\n':
@@ -262,22 +235,29 @@ while run:
 
 
     gameDisplay.fill(list(black))
-
     for i in range(len(players)):
         # draw the background street for each player
         bg_x, bg_y1, bg_y2 = showMovingRoad(bg_y1, bg_y2, len(players), i)
-
         # draw each player car
-        showCar(bg_x, players[i].location[0], players[i].location[1])
+        car_x, car_y = showCar(bg_x, players[i].location[0], players[i].location[1])
+        # draw enemy car
+        random_x, enemy_y = showEnemyCar(bg_x, random_x, enemy_y)
+        enemy_x = bg_x + random_x
 
-        #draw enemy car
-        enemy_car_startx,enemy_car_starty1,enemy_car_starty2=showEnemyCar(bg_x,enemy_car_startx,enemy_car_starty1, enemy_car_starty2)
-        
+        # Saving the values of the current client
+        if i == my_idx:
+            my_enemy_x= enemy_x
+            my_enemy_y= enemy_y
+            my_car_x=car_x
+            my_car_y=car_y
+
+        # detect collisions for current client only
+    if detectCollision(my_car_x, my_car_y, my_enemy_x, my_enemy_y):
+        display_message("Game Over")
 
     #Displaying score
-    count=highscore(count)
-    count +=1
-        
+    count = highscore(count)
+    count += 1
         
     # redraw the scene
     pygame.display.update()
